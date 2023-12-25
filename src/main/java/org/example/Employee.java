4cbca0f2058a0f50adb1c76bdf3aae7119493d70
aysubusra <aysumuratoglu@gmail.com>
2023-12-25 22:28:18 +0300

    Ödev 4

---
+++ src/main/java/org/example/Employee.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class Employee {
+
+     public String name;
+     public String surname;
+     public String position;
+     public String seniority;
+
+
+   public void personnel_full_name() {
+        System.out.println(name +" "+ surname);
+    }
+
+    public void personnel_other_infos() {
+        System.out.println(position +" "+ seniority);
+    }
+
+
+
+
+
+}
---
+++ src/main/java/org/example/EmployeeList.java
@@ -0,0 +1,27 @@
+import org.example.Employee;
+
+public class EmployeeList
+{
+    public static void main(String[] args) {
+// Employee sınıfından iki nesne oluşturuluyor
+        Employee personnel1 = new Employee();
+        Employee personnel2 = new Employee();
+// 1. çalışanın bilgileri veriliyor
+        personnel1.name = "Aysu";
+        personnel1.surname = "Murat";
+        personnel1.position = "QA Engineer";
+        personnel1.seniority = "1";
+
+// 2. çalışanın bilgileri veriliyor
+        personnel2.name = "Büşra";
+        personnel2.surname = "Murat";
+        personnel2.position = "QA Lead";
+        personnel2.seniority = "3";
+
+        personnel1.personnel_full_name();
+        personnel1.personnel_other_infos();
+        personnel2.personnel_full_name();
+        personnel2.personnel_other_infos();
+
+    }
+}
